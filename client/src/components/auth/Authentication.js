579e16d81702b65f561d93e529161f1ab1060335
chaitanya1019 <chaitanya1019@gmail.com>
2020-04-12 02:43:26 +0530

    - Registration flow bug fix

--- client/src/components/auth/Authentication.js
+++ client/src/components/auth/Authentication.js
@@ -125,7 +125,7 @@ const Authentication = ({
     } else if (
       // change active step of auth flow based on otpSent, otpVerified, token and activeStep
       (otpSent && activeStep === 0) ||
-      (otpVerified && token === null)
+      (otpVerified && token === null && activeStep === 2)
     ) {
       setActiveStep(activeStep + 1);
     } else if (activeStep === 2) {
